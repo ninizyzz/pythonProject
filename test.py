fbe1c97c71f4968c7120a9dffd09e82e66ddbdf8
ninizyzz <jurmadarius@gmail.com>
2020-11-16 20:50:53 +0200

    test

--- test.py
+++ test.py
@@ -2,9 +2,8 @@ import win32com.client
 
 strComputer = "192.168.1.128"
 objWMIService = win32com.client.Dispatch("WbemScripting.SWbemLocator")
-objSWbemServices = objWMIService.ConnectServer(strComputer,"root\cimv2")
+objSWbemServices = objWMIService.ConnectServer(strComputer, "root\cimv2")
 colItems = objSWbemServices.ExecQuery("SELECT * FROM Win32_LogicalDisk")
 
 for objItem in colItems:
     print(objItem.Size)
-
